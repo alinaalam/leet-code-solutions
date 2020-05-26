58198cb572e8c1d3aef187a8938b7e9f11474f9b
alinaalam <alinalamzeb@gmail.com>
2020-05-26 17:30:41 +0200

    Update interval-list-intersections.java

--- interval-list-intersections.java
+++ interval-list-intersections.java
@@ -4,72 +4,29 @@
 */
 class Solution {
     public int[][] intervalIntersection(int[][] A, int[][] B) {
-        // sanity check
-        if (A.length == 0 || B.length == 0) {
-            return new int[][] {};
-        }
         
+        List<int[]> result = new ArrayList<>();
         int pointer1 = 0, pointer2 = 0;
-        
-        List<Integer[]> result = new ArrayList<>();
         
         while (pointer1 < A.length && pointer2 < B.length) {
             int[] a = A[pointer1];
             int[] b = B[pointer2];
             
-            Integer[] temp = new Integer[2];
-            boolean start = false;
-            boolean end = false;
+            int start = Math.max(a[0], b[0]);
+            int end = Math.min(a[1], b[1]);
             
-            // check starting interval
-            if (b[0] == a[0]) {
-                temp[0] = b[0];
-                start = true;
-            }
-            else if (b[0] > a[0] && b[0] <= a[1]) {
-                temp[0] = b[0];
-                start = true;
-            }
-            else if (a[0] > b[0] && a[0] <= b[1]) {
-                temp[0] = a[0];
-                start = true;
+            if (start <= end) {
+                result.add(new int[]{start, end});
             }
             
-            // check ending interval
-            if (b[1] == a[1]) {
-                temp[1] = a[1];
-                end = true;
-            }
-            else if (a[1] < b[1] && a[1] >= b[0]) {
-                temp[1] = a[1];
-                end = true;
-            }
-            else if (b[1] < a[1] && b[1] >= a[0]) {
-                temp[1] = b[1];
-                end = true;
-            }
-            
-            if (start && end) {
-                result.add(temp);
-            }
-            
-            if (a[1] <= b[1]) {
+            if (a[1] < b[1]) {
                 pointer1++;
             }
-            if (b[1] <= a[1]) {
+            else {
                 pointer2++;
             }
         }
         
-        int[][] f = new int[result.size()][2];
-        int i = 0;
-        
-        for (Integer[] arr : result) {
-            int[] temp = new int[] {arr[0], arr[1]};
-            f[i] = temp;
-            i++;
-        }
-        
-        return f;
+        return result.toArray(new int[result.size()][]);
     }
 }
