623f6f18ff4ca4ba7f0e7a7da60c070bfc5eab20
alinaalam <alinalamzeb@gmail.com>
2020-07-06 00:33:19 +0200

    Update hamming-distance.java

--- hamming-distance.java
+++ hamming-distance.java
@@ -1,1 +1,17 @@
-
+/**
+* Hamming Distance
+* https://leetcode.com/problems/hamming-distance/
+*/
+class Solution {
+    public int hammingDistance(int x, int y) {
+        int count = 0;
+        
+        while (x > 0 || y > 0) {
+            count += (x % 2) ^ (y % 2);
+            x >>= 1; // x /= 2
+            y >>= 1;
+        }
+        
+        return count;
+    }
+}
