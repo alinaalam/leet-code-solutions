373641d69f0fbf4de0c318498b4f74672ce62326
alinaalam <alinalamzeb@gmail.com>
2020-07-27 02:15:52 +0200

    Update add-digits.java

--- add-digits.java
+++ add-digits.java
@@ -1,1 +1,9 @@
-
+/**
+* Add Digits
+* https://leetcode.com/problems/add-digits/
+*/
+class Solution {
+    public int addDigits(int num) {
+        return num == 0 ? 0 : 1 + (num - 1) % 9;
+    }
+}
